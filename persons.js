c42ff1bf6909e1a1d4ce8501c05c3edcb48bcc5e
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2025-03-26 21:54:22 +0600

    ok

---
+++ persons.js
@@ -0,0 +1,75 @@
+const data = [
+  {
+    name: "Ahmmed Shakib",
+    email: "26/03/2025",
+    image:
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-1/363405226_283873430905401_8570414994521243218_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=111&ccb=1-7&_nc_sid=e99d92&_nc_ohc=4v-21o-tPC4Q7kNvgFZNcU6&_nc_oc=AdnuWjW5SsyEgabkMlxtFiSvP6lyd4JQFDIyVT59uweGVlX9VQgQmEEwTELb-8Hmi3U&_nc_zt=24&_nc_ht=scontent.fdac5-1.fna&_nc_gid=cY8VnGpm2SW4zWx1UpBR6Q&oh=00_AYHjlEK7Tq7OBy11yo_iAOPvA11rDycb01w61MoaY36Bvw&oe=67E957C8",
+
+    description: "He tell me to learn saas,gsap",
+    social: {
+      linkedin: "https://www.linkedin.com/in/jashim-u75/",
+      github: "https://github.com/jashim-official75",
+      twitter: "https://x.com/jashimuddin",
+      facebook: "https://www.facebook.com/jashim.u75",
+      whatsapp: "https://wa.me/01939814448",
+    },
+    tags: ["JavaScript", "React", "Node.js", "FB"],
+    label: "Full-Stack Developer SAAS GSAP",
+  },
+  //ashik vi facebook
+
+  {
+    name: "Ashik Ahmed Shuvo  (Asik)",
+    email: "26 march 2025",
+    image:
+      "https://scontent.fdac5-2.fna.fbcdn.net/v/t1.6435-9/110962833_2760075937650385_994066683486790541_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=FS9LAxAboDUQ7kNvgHqkGW5&_nc_oc=Adm6ogogeNVXm9zO54ZTyFJkcXP_aMM9gO6FervMbjbdRYBM7uyP0EvpxelPlFLFfbM&_nc_zt=23&_nc_ht=scontent.fdac5-2.fna&_nc_gid=zSLOr1f0JtCkjOx27Efk7g&oh=00_AYFZ01A729H0HWr2EqVhA7xXExmNOjTGSsTTM3YvQUf3cw&oe=680B1B8A",
+
+    description: "ai is challenge us app development is good ",
+    social: {
+      linkedin: "https://www.linkedin.com/in/ashik360",
+      github: "https://github.com/ashik360",
+      redhat: "https://ashik360.github.io/",
+      facebook: "https://www.facebook.com/ashik360",
+      whatsapp: "https://wa.me/01795853197",
+    },
+    tags: ["JavaScript", "React", "Node.js", "Flutter", "FB"],
+    label: "Flutter",
+  },
+  // siem vi whats app
+  {
+      name: "Shohrab Hossain",
+      email: "",
+      image: "https://ibb.co.com/N6YQF2wC",
+      description: "",
+      social: {
+        // redhat: "",
+        // linkedin: "",
+        // github: "",
+        // twitter: "",
+        // facebook: "",
+        whatsapp: "https://wa.me/01742080475"
+      },
+      tags: ["JavaScript", "React", "Node.js"],
+      label: "Full-Stack Developer"
+  },
+{
+    name: "",
+    email: "",
+    image: "",
+    description: "",
+    social: {
+      redhat: "",
+      discord: "",
+      linkedin: "",
+      github: "",
+      twitter: "",
+      facebook: "",
+      whatsapp: "https://wa.me/1234567890"
+    },
+    tags: ["JavaScript", "React", "Node.js"],
+    label: "Full-Stack Developer"
+},
+
+]
+
+export default data;
