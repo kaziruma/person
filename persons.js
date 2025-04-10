ff44d0f7882ed0265440fc4c03e005cf06c76e4b
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2025-04-10 23:01:41 +0600

    update

--- persons.js
+++ persons.js
@@ -1,9 +1,26 @@
 const data = [
   {
+    name: "Sumaiya Islam",
+    email: "26/03/2025",
+    image:
+      "https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-1/469260281_505185925889088_6812828696937493662_n.jpg?stp=c0.141.1300.1300a_dst-jpg_s200x200_tt6&_nc_cat=108&ccb=1-7&_nc_sid=1d2534&_nc_eui2=AeHJIbzkmuPidfWKP2QxtqqX1rIj2298IDfWsiPbb3wgN5oQeDOWlDhCfdpFlynizOSZUuK5nql9jpD-XywWXNhU&_nc_ohc=iAcv4PbV1vEQ7kNvwE59I4a&_nc_oc=Adm7rmYAc1XDtRnZSWyOo9UzynrYScI88vM29c1dP72351ZZmMxnLV29axG7Dqk63b0&_nc_zt=24&_nc_ht=scontent.fdac5-2.fna&_nc_gid=4bmzCq_es-xNspjpkH7LgQ&oh=00_AfEWuHNaLGEZ0FVr5cgmuwjOCSzRfmER3a0KBy2RM_HDyA&oe=67FC79A0",
+
+    description: "He tell me to learn saas,gsap",
+    social: {
+      linkedin: "https://www.linkedin.com/in/sumaiya-akter-5b23a7317/",
+      github: "https://github.com/sumayan7477",
+      // twitter: "https://x.com/jashimuddin",
+      facebook: "https://www.facebook.com/sumaiya.islam.72019/",
+      whatsapp: "https://wa.me/01939814448",
+    },
+    tags: ["JavaScript", "React", "Node.js", "FB", "SC"],
+    label: "Full-Stack Developer SAAS GSAP",
+  },
+  {
     name: "Ahmmed Shakib",
     email: "26/03/2025",
     image:
-      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-1/363405226_283873430905401_8570414994521243218_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=111&ccb=1-7&_nc_sid=e99d92&_nc_ohc=4v-21o-tPC4Q7kNvgFZNcU6&_nc_oc=AdnuWjW5SsyEgabkMlxtFiSvP6lyd4JQFDIyVT59uweGVlX9VQgQmEEwTELb-8Hmi3U&_nc_zt=24&_nc_ht=scontent.fdac5-1.fna&_nc_gid=cY8VnGpm2SW4zWx1UpBR6Q&oh=00_AYHjlEK7Tq7OBy11yo_iAOPvA11rDycb01w61MoaY36Bvw&oe=67E957C8",
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-6/482031891_637137435578997_6125483762022220601_n.jpg?stp=dst-jpg_s960x960_tt6&_nc_cat=109&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeFOHV-Z6tWo4ZvYcvYayG6legY9xGEiULl6Bj3EYSJQuYOYwphJCMYTgdGRdHRel1_55xZfNzpNqaklX2x3GsoF&_nc_ohc=NbQVsV6pYcoQ7kNvwEptbX3&_nc_oc=AdmsitWB9Z9rB2YBhTCdAdU5fGc3DOAimnE55ec36A204ksBnFI2mCEZiKSOXbEvuwI&_nc_zt=23&_nc_ht=scontent.fdac5-1.fna&_nc_gid=v9iRhdTf-VVwfvDB6_X6ug&oh=00_AfGmmWJW-ld4M_btKGsCD54MR9BbA2EvsN4Vh2WDbMg3NA&oe=67FC8942",
 
     description: "He tell me to learn saas,gsap",
     social: {
@@ -37,40 +54,57 @@ const data = [
   },
   // siem vi whats app
   {
-      name: "Shohrab Hossain",
-      email: "discord creator",
-      image: "https://i.ibb.co.com/8D8TmgFs/image.png",
-      description: "",
-      social: {
-        // redhat: "",
-        // linkedin: "",
-        // github: "",
-        // twitter: "",
-        // facebook: "",
-        whatsapp: "https://wa.me/01742080475"
-      },
-      tags: ["JavaScript", "React", "Node.js"],
-      label: "Full-Stack Developer"
+    name: "Shohrab Hossain",
+    email: "discord creator",
+    image: "https://i.ibb.co.com/8D8TmgFs/image.png",
+    description: "",
+    social: {
+      // redhat: "",
+      // linkedin: "",
+      // github: "",
+      // twitter: "",
+      // facebook: "",
+      whatsapp: "https://wa.me/01742080475",
+    },
+    tags: ["JavaScript", "React", "Node.js"],
+    label: "Full-Stack Developer",
   },
   {
-      name: "Abdur Rahim  (Coder)",
-      email: "create here own business",
-      image: "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-1/482086227_1964083400790094_3203297488187245822_n.jpg?stp=c162.0.795.795a_dst-jpg_s200x200_tt6&_nc_cat=110&ccb=1-7&_nc_sid=1d2534&_nc_ohc=2EkVVTEfHyMQ7kNvgEbIHnI&_nc_oc=Adl6IPzIP1nz-1t-X2VawHsaIlrdb6G1YAhIO_N30fPmBSCHz-VmaQweuR_T_3SxhIo&_nc_zt=24&_nc_ht=scontent.fdac5-1.fna&_nc_gid=TtQYUbvAWLCca0IOlTUOIw&oh=00_AYHC19nyh_TRT3-BvCRli3_H3LtZtgW_DqORGXfpScdGKA&oe=67E9EE37",
-      description: "",
-      social: {
-        redhat: "https://coderrahim.me/",
-        // discord: "",
-        // linkedin: "",
-        github: "https://github.com/coderrahim",
-        // twitter: "",
-        facebook: "https://www.facebook.com/CoderRahimBD",
-        whatsapp: "https://wa.me/01609033435"
-      },
-      tags: ["JavaScript", "React", "Node.js"],
-      label: "Full-Stack Developer"
+    name: "Abdur Rahim  (Coder)",
+    email: "create here own business",
+    image:
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-1/482086227_1964083400790094_3203297488187245822_n.jpg?stp=c162.0.795.795a_dst-jpg_s200x200_tt6&_nc_cat=110&ccb=1-7&_nc_sid=1d2534&_nc_eui2=AeHiazlx8L1B7O1x07HREdGOo6pQ1UTE89GjqlDVRMTz0ROTpKwctd1p3iFQlPuv8mFXxNZsWErTxEk8_sjeXyBj&_nc_ohc=FwpL6gsG84oQ7kNvwELIFeE&_nc_oc=AdmVGQtYj89VgrCEVxlGkMh8ILO-gEwFw-oktm0583hniuvVjg9HRilFrsoDRJsoxTo&_nc_zt=24&_nc_ht=scontent.fdac5-1.fna&_nc_gid=qwU9uj0Iop2kj8dFaoHzsA&oh=00_AfFpmA4Dz1k9uQrHhcJN49Rp2vy194eo3FJHDtfQXeN2Tg&oe=67FC6337",
+    description: "",
+    social: {
+      redhat: "https://coderrahim.me/",
+      // discord: "",
+      // linkedin: "",
+      github: "https://github.com/coderrahim",
+      // twitter: "",
+      facebook: "https://www.facebook.com/CoderRahimBD",
+      whatsapp: "https://wa.me/01609033435",
+    },
+    tags: ["JavaScript", "React", "Node.js"],
+    label: "Full-Stack Developer",
   },
-
-
-]
+  {
+    name: "Tipu Sultan",
+    email: "create here own business",
+    image:
+      "https://scontent.fdac5-1.fna.fbcdn.net/v/t39.30808-1/482086227_1964083400790094_3203297488187245822_n.jpg?stp=c162.0.795.795a_dst-jpg_s200x200_tt6&_nc_cat=110&ccb=1-7&_nc_sid=1d2534&_nc_eui2=AeHiazlx8L1B7O1x07HREdGOo6pQ1UTE89GjqlDVRMTz0ROTpKwctd1p3iFQlPuv8mFXxNZsWErTxEk8_sjeXyBj&_nc_ohc=FwpL6gsG84oQ7kNvwELIFeE&_nc_oc=AdmVGQtYj89VgrCEVxlGkMh8ILO-gEwFw-oktm0583hniuvVjg9HRilFrsoDRJsoxTo&_nc_zt=24&_nc_ht=scontent.fdac5-1.fna&_nc_gid=qwU9uj0Iop2kj8dFaoHzsA&oh=00_AfFpmA4Dz1k9uQrHhcJN49Rp2vy194eo3FJHDtfQXeN2Tg&oe=67FC6337",
+    description: "",
+    social: {
+      // redhat: "https://coderrahim.me/",
+      // discord: "",
+      // linkedin: "",
+      // github: "https://github.com/coderrahim",
+      // twitter: "",
+      facebook: "https://www.facebook.com/tipusultan1937",
+      whatsapp: "https://wa.me/01609033435",
+    },
+    tags: ["JavaScript", "React", "Wordpress"],
+    label: "Full-Stack Developer",
+  },
+];
 
 export default data;
